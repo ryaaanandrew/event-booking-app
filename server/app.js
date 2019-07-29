d9d9880a442606330c8ad9955728d8de35a6a15b
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-07-28 20:06:10 -0400

    graphql set up

--- server/app.js
+++ server/app.js
@@ -1,6 +1,36 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 const PORT = 4000;
+
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    trueValue: {},
+    rootValue: {
+        events: () => {
+            return ['Romantic Cooking', 'Sailing', 'All night coding']
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
