b861744079191f07818cd8a1781e26cce3cf556b
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-07-28 21:47:05 -0400

    connected to mongoDB and created event schema via mongooose

--- server/app.js
+++ server/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
+
 
 const app = express();
 const PORT = 4000;
+
+const mango = 'mongodb+srv://<username>:<password>@cluster0-8nozd.mongodb.net/test?retryWrites=true&w=majority'
 
 const events = [];
 
@@ -43,13 +49,19 @@ app.use('/graphql', graphqlHTTP({
             return events
         },
         createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
+            // const event = {
+            //     _id: Math.random().toString(),
+            //     title: args.eventInput.title,
+            //     description: args.eventInput.description,
+            //     price: +args.eventInput.price,
+            //     date: args.eventInput.date
+            // }
+            const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: args.eventInput.date
-            }
+                date: new Date(args.eventInput.date)
+            });
             events.push(event);
             return event;
         }
@@ -57,4 +69,8 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }));
 
-app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-8nozd.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`, { useNewUrlParser: true }
+    )
+    .then(app.listen(PORT, () => console.log(`Connected to MongoDB and now listening on port ${PORT}`)))
+    .catch(err => console.log(err));
