5c155e3223a707cf90ecdedd416b5f0125db3665
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-07-31 14:17:03 -0400

    added fuctionality to login/signup page

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,9 +1,95 @@
 import React, { Component } from 'react';
 
 class Auth extends Component {
+    state = {
+        isLogIn: true
+    }
+    constructor(props) {
+        super(props);
+        this.emailEl = React.createRef();
+        this.passwordEl = React.createRef();
+    }
+    
+    submitHandler = e => {
+        e.preventDefault();
+        const email = this.emailEl.current.value;
+        const password = this.passwordEl.current.value;
+
+        if(email.trim().length === 0 || password.trim().length === 0) {
+            return;
+        };
+
+        let requestBody = {
+            query: `
+                query {
+                    login(email: "${email}", password: "${password}") {
+                        userId
+                        token
+                        tokenExpiration
+                    }
+                }
+            `
+        };
+
+        if(!this.state.isLogIn) {
+            requestBody = {
+                query: `
+                    mutation {
+                        createUser(userInput:{email:"${email}", password:"${password}"}){
+                            _id
+                            email
+                        }
+                    }
+                `
+            };
+        };
+
+        
+
+        fetch('http://localhost:4000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => {
+            if(res.status !== 200 && res.status !== 201) {
+                throw new Error('failed');
+            }
+            return res.json();
+        })
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    };
+
+    switchModeHandler = () => {
+        this.setState(prevProps => {
+            return { isLogIn: !prevProps.isLogIn };
+        })
+    }
+
     render() {
         return (
-            <div>Auth page</div>
+            <form className='authform' onSubmit={e => this.submitHandler(e)}>
+                <h1>{ this.state.isLogIn ? 'Log in' : 'Sign up'}</h1>
+                <div className="authform__control">
+                    <label htmlFor="email">Email</label>
+                    <input type="text" id='email' ref={this.emailEl} />
+                </div>
+                <div className="authform__control">
+                    <label htmlFor="password">password</label>
+                    <input type="text" id='password' ref={this.passwordEl} />
+                </div>
+                <div className="authform__actions">
+                <button type='submit'>Submit</button>
+                <button type='button' onClick={this.switchModeHandler}>Switch to { this.state.isLogIn ? 'Sign up' : 'Log in'}</button>
+                </div>
+            </form>
         );
     };
 };
