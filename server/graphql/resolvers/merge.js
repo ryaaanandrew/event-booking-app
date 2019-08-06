9085ed7316704ca51a1dbe3969ea8b3a48c425bb
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-08-05 22:42:29 -0400

    fixed bug in events resolver

--- server/graphql/resolvers/merge.js
+++ server/graphql/resolvers/merge.js
@@ -16,6 +16,10 @@ const userLoader = new DataLoader((userIds) => {
 const events = async eventIds => {
     try {
       const events = await Event.find({ _id: { $in: eventIds } });
+      events.sort((a,b) => {
+        return eventIds.indexOf(a._id.toString()) - eventIds.indexOf(b._id.toString());
+      });
+
       return events.map(event => transformEvent(event));
     } catch (err) {
       throw err;
